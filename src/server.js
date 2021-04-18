7fadf7f690ae3879020c1227c5c38446374f56d5
Gjergj Lala <g.lala@lalatech.de>
2021-04-18 19:25:11 +0200

    Rewriting the upvote endpoint

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-import express    from 'express';
+import express         from 'express';
 import { MongoClient } from 'mongodb';
 
 // Mongodb authentication
@@ -11,24 +11,37 @@ const app = express();
 app.use(express.json());
 
 app.get('/api/articles/:name', async (req, res) => {
+  const client = await MongoClient.connect(`mongodb://${mongoUser}:${mongoPassword}@localhost:27017`, { useUnifiedTopology: true });
   try {
     const articleName = req.params.name;
-    const client = await MongoClient.connect(`mongodb://${mongoUser}:${mongoPassword}@localhost:27017`, { useNewUrlParser: true });
+
     const db = client.db('my-blog');
-    const articleInfo = await db.collection('articles').findOne({name: articleName});
   
-    res.status(200).json(articleInfo);
+    const articleInfo = await db.collection('articles').findOne({ name: articleName });
+    res.status(200).json(articleInfo); 
   } catch (error) {
     res.status(500).json({ message: 'Error connecting to db', error });
   } finally {
     client.close();
-  }  
+  }
 });
 
-app.post( '/api/articles/:name/upvote', (req, res) => {
-  const articleName = req.params.name;
-  articleInfo[articleName].upvotes +=1;
-  res.status(200).send(`${articleName} now has ${articleInfo[articleName].upvotes} upvotes!`);
+app.post('/api/articles/:name/upvote', async (req, res) => {
+  const client = await MongoClient.connect(`mongodb://${mongoUser}:${mongoPassword}@localhost:27017`, { useUnifiedTopology: true });
+  try {
+    const articleName = req.params.name;
+    const articles = client.db('my-blog').collection('articles');
+  
+    const articleInfo        = await articles.findOne({ name: articleName });
+    await articles.updateOne({ name: articleName }, { '$set': { upvotes: articleInfo.upvotes +1 } });
+    const updatedArticleInfo = await articles.findOne({ name: articleName });
+
+    res.status(200).json(updatedArticleInfo);
+  } catch (error) {
+    res.status(500).json({ message: 'Error connecting to db', error });
+  } finally {
+    client.close();
+  }
 });
 
 app.post('/api/articles/:name/add-comment', (req, res) => {
@@ -36,7 +49,6 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
   const articleName = req.params.name;
   articleInfo[articleName].comments.push({ username, text });
   res.status(200).send(articleInfo[articleName]);
-
-})
+});
 
 app.listen('8000', console.log('Listening on port 8000'));
